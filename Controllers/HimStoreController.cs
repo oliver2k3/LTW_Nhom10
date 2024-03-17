796e7e4628e07204de45b50e6f1e561f79338489
oliver2k3 <olivergaming9889@gmail.com>
2024-03-17 22:57:16 +0700

    Add project files.

---
+++ Controllers/HimStoreController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LTW_Nhom10.Controllers
+{
+    public class HimStoreController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
